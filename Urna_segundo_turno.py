ef671d3972de5b5b38303c6872630d4d19c2b284
Luis Gabriel Oliveira <97142989+luisgabriel1@users.noreply.github.com>
2022-10-12 00:44:50 -0300

    Update Urna_segundo_turno.py

--- Urna_segundo_turno.py
+++ Urna_segundo_turno.py
@@ -1,9 +1,6 @@
-votos = []
-lula = []
-bolsonaro = []
+votos, lula, bolsonaro = [], [], []
 while True:
-    pesquisa = input(
-        'Lula ou Bolsonaro  [ L / B ]. Para sair envie S\n').upper()
+    pesquisa = input('Lula ou Bolsonaro  [ L / B ]. Para sair envie S\n').upper()
     votos += pesquisa
 
     if pesquisa == 'L':
@@ -14,8 +11,7 @@ while True:
 
     elif pesquisa == 'S':
         votos_1 = int(len(votos) - len(pesquisa))
-        calc_b = int((len(bolsonaro) / votos_1) * 100)
-        calc_l = int((len(lula) / votos_1) * 100)
-        print(
-            f'Lula está com {...}% votos.\nBolsonaro está com {calc_b}% de votos.')
+        calc_b, calc_l = int((len(bolsonaro) / votos_1) * 100), int((len(lula) / votos_1) * 100)
+        print(f'Lula está com {calc_l}% votos.\nBolsonaro está com {calc_b}% de votos.')
         break
+
